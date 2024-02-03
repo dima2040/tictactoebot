efcd0c0788df4da10b6fde37e8152ac939388429
Dima Nekrasov <okulelelelele@gmail.com>
2024-02-03 15:20:49 +0300

    Добавление поддержки разных языков

--- tictactoebot/translate.py
+++ tictactoebot/translate.py
@@ -1,16 +1,16 @@
 import json
 import os
 
+
 translations_dir = os.path.dirname(os.path.dirname(__file__)) + "/translations/"
+
 
 def save_user_languages(languages: dict) -> None:
     json.dump(languages, open("languages.json", "w"))
 
+
 def load_user_languages() -> dict:
     return  json.load(open("languages.json"))
-
-
-
 
 
 def get_languages() -> list:
@@ -20,21 +20,21 @@ def get_languages() -> list:
         langs.append(name)
     return langs
 
+
 def get_languages_dict() -> dict:
     langs = dict()
     for file in os.listdir(translations_dir):
         code = file.replace(".json", "")
-        data = json.load(open(f"{translations_dir}{file}"))
+        with open(f"{translations_dir}{file}") as f:
+            data = json.load(f)
         name = data['name']
         langs[code] = name
     return langs
 
 
-
-
-
 def is_lang_supported(name: str) -> bool:
     return name in get_languages()
+
 
 def find_translate(name: str) -> dict:
     for file in os.listdir(translations_dir):
@@ -42,6 +42,7 @@ def find_translate(name: str) -> dict:
         if name == filename:
             return json.load(open(f"{translations_dir}{file}"))
     return {}
+
 
 def get_translate(name: str) -> dict:
     if is_lang_supported(name):
